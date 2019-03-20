58940bb070d32ff5d3c6f2d8958f4cffca16c642
K.Prusinskas <karolis.prusinskas@gmail.com>
2019-03-20 08:04:25 +0200

    Updates

---
+++ precache-manifest.eb43143e1c209952aac6bcce4de0071b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3535e2b2e554449540e",
+    "url": "/pt-unsplash-api/static/js/runtime~main.5ce92dd7.js"
+  },
+  {
+    "revision": "a5190af9ef0e7cfba24f",
+    "url": "/pt-unsplash-api/static/js/main.38e71e76.chunk.js"
+  },
+  {
+    "revision": "73080c8c5313bf1fb476",
+    "url": "/pt-unsplash-api/static/js/2.d519b009.chunk.js"
+  },
+  {
+    "revision": "a5190af9ef0e7cfba24f",
+    "url": "/pt-unsplash-api/static/css/main.15b19f50.chunk.css"
+  },
+  {
+    "revision": "d5ebef57cca810198792b98d1e74b29f",
+    "url": "/pt-unsplash-api/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pt-unsplash-api/precache-manifest.eb43143e1c209952aac6bcce4de0071b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pt-unsplash-api/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
